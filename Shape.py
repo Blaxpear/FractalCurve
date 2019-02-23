f0f87d7ea50400f57a127d2c47dbd38f4aedb2ea
Blaxpear <tatu.holkko@gmail.com>
2019-02-24 00:09:20 +0200

    automatic linking command

--- Shape.py
+++ Shape.py
@@ -14,35 +14,17 @@ class Shape:
         # list of Vertex object pairs to only draw lines
         self.visuals = []
 
-    def link_vertices(self, vtxlist):
-        self.length = vtxlist[0].distanceTo(vtxlist[-1])
-        self.start = vtxlist[0]
-        self.end = vtxlist[-1]
-        self.set_zeroshape(vtxlist)
-        self.links = []
-        self.link_adjacent(vtxlist)
-        self.visuals = []
-
-    def link_adjacent(self, vtxlist):
-        """
-        Link each vertex to the one next to it
-        """
-        for i in range(len(vtxlist) - 1):
-            if i % 2 == 0:
-                self.links.append(self.get_link(vtxlist[i], vtxlist[i + 1], mirror_y=True, mirror_x=False))
-            else:
-                self.links.append(self.get_link(vtxlist[i], vtxlist[i + 1], mirror_y=False, mirror_x=False))
-
-    def get_link(self, vtx1, vtx2, mirror_x, mirror_y) -> Link:
+    def link(self, vtx1, vtx2, mirror_x, mirror_y):
         """
         Return a link between these vertices
         :param vtx1: first vertex
         :param vtx2: second vertex
-        :return: Link object
+        :param mirror_x: boolean
+        :param mirror_y: boolean
         """
         d = vtx1.distanceTo(vtx2)
         scale = d / self.length
-        return Link(vtx1, vtx2, scale, self, mirror_x=mirror_x, mirror_y=mirror_y)
+        self.links.append(Link(vtx1, vtx2, scale, self, mirror_x=mirror_x, mirror_y=mirror_y))
 
     def draw(self, stage, graphics) -> bool:
         """
--- ShapeFileHandler.py
+++ ShapeFileHandler.py
@@ -9,7 +9,8 @@ class ShapeFileHandler:
         self.commands = {'vertex': self.add_vertex,
                          'start': self.set_start,
                          'end': self.set_end,
-                         'link': self.add_link}
+                         'link': self.add_link,
+                         'link all': self.link_all}
         self.vertices = None
         self.shape = None
         self.manual_zeropos = False
@@ -28,22 +29,25 @@ class ShapeFileHandler:
 
             columns = line.split(':')
             rowtype = columns[0]
-
+            arguments = columns[1]
             for type, function in self.commands.items():
                 if rowtype == type:
-                    function(columns)
+                    function(arguments)
                     break
 
         if not self.manual_zeropos:
             self.shape.set_zeroshape(self.vertices)
         return self.shape
 
-    def add_vertex(self, columns):
+    def add_vertex(self, arguments):
         """
-        Read columns and save vertex into self.vertices
-        :param columns: columns of the row
+        Save vertex into self.vertices
+        :param arguments:
+        string that contains: "x, y; x0, y0"
+        x, y are full stage position. required
+        x0, y0 are zero stage position. optional, if omitted no semicolon should follow y argument
         """
-        coordinates = columns[1].split(';')
+        coordinates = arguments.split(';')
         if not self.manual_zeropos and len(coordinates) == 2:
             # two coordinates given, so zero positions are manually set
             self.manual_zeropos = True
@@ -63,39 +67,39 @@ class ShapeFileHandler:
             x, y = coordinates[0].split(',')
             self.vertices.append(Vertex(float(x), float(y)))
 
-    def set_start(self, columns):
+    def set_start(self, index):
         """
         Set the vertex that will be drawn to the origin of the link.
-        :param columns: columns of the row
-        Columns's second element must be an integer index (starting from 1) of the previously saved vertex
+        :param index:
+        A string containing integer index (starting from 1) referring to previously saved vertex
         """
-        setting = columns[1].strip().lower()
+        setting = index.strip().lower()
         if setting == 'first':
             vtx_index = 1
         else:
-            vtx_index = int(columns[1])
+            vtx_index = int(index)
         self.shape.start = self.vertices[vtx_index - 1]
 
-    def set_end(self, columns):
+    def set_end(self, index):
         """
         Set the vertex that will be drawn to the end of the link and calculate the length of the shape.
         Start vertex must be defined before calling this.
-        :param columns: columns of the row
-        Columns's second element must be an integer index (starting from 1) of the previously saved vertex.
+        :param index:
+        A string containing integer index (starting from 1) referring to previously saved vertex
         """
-        setting = columns[1].strip().lower()
+        setting = index.strip().lower()
         if setting == 'last':
             vtx_index = len(self.vertices)
         else:
-            vtx_index = int(columns[1])
+            vtx_index = int(index)
         self.shape.end = self.vertices[vtx_index - 1]
         self.shape.length = self.shape.start.distanceTo(self.shape.end)
 
-    def add_link(self, columns):
+    def add_link(self, arguments):
         """
         Add a link into self.shape
-        :param columns: columns of the row
-        Columns's second element is a list delimited by ';'
+        :param arguments:
+        A string that has elements delimited by ';'
         elements:
         1: index of first vertex (starting from 1)
         2: index of second vertex
@@ -103,10 +107,26 @@ class ShapeFileHandler:
         4: 0/1 boolean integer for mirroring y-axis
         vertices at those indexes must be defined before calling this
         """
-        ind1, ind2, mir_x, mir_y = columns[1].split(';')
+        ind1, ind2, mir_x, mir_y = arguments.split(';')
         ind1 = int(ind1) - 1
         ind2 = int(ind2) - 1
         mir_x = mir_x.strip() == '1'
         mir_y = mir_y.strip() == '1'
         self.shape.links.append(self.shape.get_link(self.vertices[ind1], self.vertices[ind2], mir_x, mir_y))
 
+    def link_all(self, arguments):
+        """
+        Link all vertices in the order they were saved
+        :param arguments:
+        A string that has elements delimited by ';'
+        elements:
+        1: 0/1 boolean integer for y-mirroring all odd links
+        2: 0/1 boolean integer for y-mirroring all even links
+        """
+        mir_y_odd = arguments.split(';')[0].strip() == '1'
+        mir_y_even = arguments.split(';')[1].strip() == '1'
+        for i in range(len(self.vertices) - 1):
+            if i % 2 == 0:
+                self.shape.link(self.vertices[i], self.vertices[i + 1], mirror_y=mir_y_even, mirror_x=False)
+            else:
+                self.shape.link(self.vertices[i], self.vertices[i + 1], mirror_y=mir_y_odd, mirror_x=False)
